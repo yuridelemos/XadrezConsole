1aacdf7be481befd8ad454c01907a623c1f5aa36
Yuri de Lemos <yuridelemos@outlook.com>
2022-04-22 14:32:06 -0300

    Comecando a implementar o tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -7,9 +7,7 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3, 4);
-            Console.WriteLine("Posicao " + P);
+            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
         }
     }
 }
---
+++ Xadrez/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int coluna { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.coluna = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+    }
+}
