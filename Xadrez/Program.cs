7411fd3ed030fcbb7574b075e169e3478a325005
Yuri de Lemos <yuridelemos@outlook.com>
2022-04-26 23:13:50 -0300

    Criando a classe PosicaoXadrez

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -8,19 +8,11 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 9));
-                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 0));
-                Tela.imprimirTabuleiro(tabuleiro);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.toPosicao());
         }
     }
 }
---
+++ Xadrez/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+            //'a' = internamente é um número inteiro. Se for 'a' - 'a' = 0, se for o B é o código do B menos o código do A e assim por diante
+        }
+        public override string ToString()
+        {
+            return "" + coluna + linha; //"" força converter para string    
+        }
+    }
+}
